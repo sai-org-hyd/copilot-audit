31a966e0e58b5fd8eed921a67dd68df308914052
sai <bhavannarayan1994@gmail.com>
2025-05-17 10:07:49 +0530

    sec3

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ const port = 3000;
 
 
 app.get('/hello', (req, res) => {
-  const password = req.query.password;
+  const userInput = req.query.password;
 
-  const filename = `./uploads/${password}`; 
+  const basePath = './uploads/';
+  const filename = path.join(basePath, userInput); 
+  console.log(`Attempting to access file: ${filename}`); 
   try {
     if (fs.existsSync(filename)) {
       const data = fs.readFileSync(filename, 'utf8');
-       res.send(data);
-    }
-    else{
-        res.send("File not found");
+      res.send(data);
+    } else {
+      res.send("File not found");
     }
   } catch (e) {
     console.error(e);
     res.status(500).send('Internal Server Error');
   }
-
 });
 
 app.listen(port, () => {
